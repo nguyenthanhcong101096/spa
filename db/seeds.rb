5c53fffd24b25dd30fc98fa30d21d977b691bc6f
congnt <cong.nguyen@coinhako.com>
2022-07-18 17:10:09 +0700

    swagger for order buy sell swap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+if AdminUser.count < 1
+  AdminUser.find_or_create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+end
 
 5.times do |i|
   location = "public/ashion/img/categories/category-#{i + 1}.jpg"
