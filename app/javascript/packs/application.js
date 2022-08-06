8a9a5b011c69fd1cd54131c6f0dccd6cfc62c9b8
congnt <cong.nguyen@coinhako.com>
2022-08-06 21:45:22 +0700

    fix ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,14 +4,15 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import "../stylesheets/application.scss"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
+
 import * as Routes from '../routes';
 window.Routes = Routes;
 
