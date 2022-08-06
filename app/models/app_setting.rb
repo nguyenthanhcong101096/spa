631ef7138e485feff33d31147fea246b4ca94b4c
congnt <cong.nguyen@coinhako.com>
2022-08-06 13:43:26 +0700

    app_settings

--- app/models/app_setting.rb
+++ app/models/app_setting.rb
@@ -1,3 +1,9 @@
 class AppSetting < ApplicationRecord
   mount_uploader :logo, ImageUploader
+
+  def logo_url
+    return logo.url unless AppSetting.last.dummy_image
+
+    "/ashion/img/logo.png"
+  end
 end
