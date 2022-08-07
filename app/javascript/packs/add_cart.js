2d9d6af13a38e8e6927f8291d0165a761ae2e2e4
congnt <cong.nguyen@coinhako.com>
2022-08-07 12:22:57 +0700

    add to cart

--- app/javascript/packs/add_cart.js
+++ app/javascript/packs/add_cart.js
@@ -2,14 +2,20 @@ const $ = require('jquery')
 
 function handleDataProduct() {
   const product = $(this).data();
+  const numberCart = $('.number-cart')
+
   const shopCart = JSON.parse(localStorage.getItem('shop_cart'))
+
+  numberCart.removeClass('hidden')
 
   if (Array.isArray(shopCart)) {
     shopCart.push(product)
 
     localStorage.setItem('shop_cart', JSON.stringify(shopCart))
+    numberCart.text(shopCart.length)
   } else {
     localStorage.setItem('shop_cart', JSON.stringify([product]))
+    numberCart.text('1')
   }
 }
 
@@ -19,6 +25,17 @@ function addToCart() {
   btnAddToCart.on('click', handleDataProduct)
 }
 
+function showNumberAddCart() {
+  const numberCart = $('.number-cart')
+  const shopCart = JSON.parse(localStorage.getItem('shop_cart'))
+
+  if(shopCart == null) return;
+
+  numberCart.removeClass('hidden')
+  numberCart.text(shopCart.length)
+}
+
 $(function () {
+  showNumberAddCart();
   addToCart();
 })
