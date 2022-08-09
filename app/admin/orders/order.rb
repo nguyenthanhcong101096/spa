682c7f4f0e8b57adaf46fbd4d3eee4f7dfd4a28e
congnt <cong.nguyen@coinhako.com>
2022-08-09 23:21:58 +0700

    order

--- app/admin/orders/order.rb
+++ app/admin/orders/order.rb
@@ -25,39 +25,45 @@ ActiveAdmin.register Order do
   end
 
   show do
-    attributes_table do
-      row :full_name 
-      row :address
-      row :phone
-      row :note
-      row :created_at
-      row :total_price do |record|
-        number_to_currency(record.order_products.sum(:total_price), unit: "VNĐ", separator: ",", delimiter: ".", format: "%n %u")
-      end
-    end
-
-    tabs do 
-    end
-
-    order.order_products.map do |record|
-      panel record.product_name do
-        attributes_table_for record do
-          row :name do
-            link_to(record.product_name, admin_product_path(record.product_id))
-          end
-
-          row :image do |record|
-            image_tag(record.product_image.url.to_s, height: '200', width: '300')
-          end
-
-          row :price do |record|
-            number_to_currency(record.price, unit: "VNĐ", separator: ",", delimiter: ".", format: "%n %u")
-          end
-
-          row :quantity
-
+    columns do
+      column do
+        para "Thông tin đơn hàng"
+        attributes_table do
+          row :full_name 
+          row :address
+          row :phone
+          row :note
+          row :created_at
           row :total_price do |record|
-            number_to_currency(record.total_price, unit: "VNĐ", separator: ",", delimiter: ".", format: "%n %u")
+            number_to_currency(record.order_products.sum(:total_price), unit: "VNĐ", separator: ",", delimiter: ".", format: "%n %u")
+          end
+        end
+      end
+
+      column do
+        para "Sản phẩm"
+
+        order.order_products.map do |record|
+          panel record.product_name do
+            attributes_table_for record do
+              row :name do
+                link_to(record.product_name, admin_product_path(record.product_id))
+              end
+    
+              row :image do |record|
+                image_tag(record.product_image.url.to_s, height: '200', width: '300')
+              end
+    
+              row :price do |record|
+                number_to_currency(record.price, unit: "VNĐ", separator: ",", delimiter: ".", format: "%n %u")
+              end
+    
+              row :quantity
+    
+              row :total_price do |record|
+                number_to_currency(record.total_price, unit: "VNĐ", separator: ",", delimiter: ".", format: "%n %u")
+              end
+            end
           end
         end
       end
